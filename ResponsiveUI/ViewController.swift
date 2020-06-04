e60618747ad1813ba0f2d48f524a44cb0312774a
Samsul Hadi <samsulhadikediri@gmail.com>
2020-06-04 15:48:44 +0700

    Initial Commit

---
+++ ResponsiveUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResponsiveUI
+//
+//  Created by SAMSUL HADI on 04/06/20.
+//  Copyright © 2020 SAMSUL HADI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
